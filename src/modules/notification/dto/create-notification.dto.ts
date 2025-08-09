b33349aba3ed755d4e9a446c91b2eb90555b0a7f
Viet Dung <vietdung281104@gmail.com>
2025-08-10 02:58:49 +0700

    decorator swagger for noti

--- src/modules/notification/dto/create-notification.dto.ts
+++ src/modules/notification/dto/create-notification.dto.ts
@@ -20,8 +20,13 @@
 // }
 
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateNotificationDto {
+  @ApiProperty({
+    example: 'johndoe@example.com',
+    description: 'Email address to subscribe',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
